2abafb10c93fff6e5954870fc1874d4cc34c5d4d
peterfisher <c3T-y7J-vo3-qpg>
2020-12-14 13:09:10 +0000

    Adding tests

--- src/__tests__/SimpleImageGridItem.test.js
+++ src/__tests__/SimpleImageGridItem.test.js
@@ -1,22 +1,37 @@
 import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import SimpleImageGridItem from '../Components/SimpleImageGridItem'
 import '@testing-library/jest-dom/extend-expect'
 
-const item = {
-  link: {
-    title: 'PHP Array Chunk',
-    target: '_blank',
-    url: 'https://youtu.be/LSDmrLmVbZI'
-  },
-  image: {
-    title: 'PHP Array Chunk',
-    alt: 'Learn PHP Array Chunk',
-    url: 'http://i3.ytimg.com/vi/LSDmrLmVbZI/maxresdefault.jpg'
+test('loads image grid item with default values', () => {
+  const item = {
+    link: {
+      url: 'https://youtu.be/LSDmrLmVbZI'
+    },
+    image: {
+      alt: 'PHP Array Chunk',
+      url: 'http://i3.ytimg.com/vi/LSDmrLmVbZI/maxresdefault.jpg'
+    }
   }
-}
+  render(<SimpleImageGridItem item={item}/>)
+  expect(screen.getByRole('img')).toHaveAttribute('src', item.image.url)
+  expect(screen.getByRole('img')).toHaveAttribute('alt', item.image.alt)
+  expect(screen.getByRole('link')).toHaveAttribute('href', item.link.url)
+})
 
-test('loads image grid item',() => {
+test('loads image grid item with optional values', () => {
+  const item = {
+    link: {
+      title: 'PHP Array Chunk',
+      target: '_blank',
+      url: 'https://youtu.be/LSDmrLmVbZI'
+    },
+    image: {
+      title: 'PHP Array Chunk',
+      alt: 'Learn PHP Array Chunk',
+      url: 'http://i3.ytimg.com/vi/LSDmrLmVbZI/maxresdefault.jpg'
+    }
+  }
   render(<SimpleImageGridItem item={item}/>)
   expect(screen.getByRole('img')).toHaveAttribute('src', item.image.url)
   expect(screen.getByRole('img')).toHaveAttribute('alt', item.image.alt)
